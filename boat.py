3ccd79543def916e6373244dc260ecb06706cdd8
Anthony Robinson <anthonyrobinson@Anthonys-MacBook-Pro.local>
2022-06-04 21:31:43 -0700

    Added self link to boats, implemented GET /boats/:boat_id

--- boat.py
+++ boat.py
@@ -75,6 +75,7 @@ def get_user_boats(owner_id, request):
 
     for boat in results:
         boat['id'] = boat.key.id
+        boat['self'] = request.base_url + "/" + str(boat.key.id)
     output = {"boats": results}
     if next_url:
         output['next'] = next_url
@@ -115,48 +116,6 @@ def get_post_boats():
         client.put(new_boat)
         myRequest['id'] = str(new_boat.key.id)
         return(myRequest, 201)
-
-# def get_post_boats():
-#     if request.method == 'GET':
-#         #query the datastore
-#         if 'credentials' not in session:
-#             res = get_public_boats()
-#             return (json.dumps(res), 200)
-#         else:
-#             token = session['credentials']['id_token']
-#             idinfo = id_token.verify_oauth2_token(token, grequests.Request(), CLIENT_ID)
-#             ownerid = idinfo['sub']
-#             res = get_boats_from_owner(ownerid)
-#             return (json.dumps(res, indent=4), 200)
-#     elif request.method == 'POST':
-#         #make sure jwt authenticated
-#         try:
-#             bearer = request.headers.get('Authorization')
-#             if bearer:
-#                 token = bearer.split()[1]
-#             else:
-#                 return("Error: unable to create boat, check JWT", 401)
-#             idinfo = id_token.verify_oauth2_token(token, grequests.Request(), CLIENT_ID)
-#             userid = idinfo['sub']
-#             #put in the datastore
-#             content = request.get_json()
-#             new_boat = datastore.entity.Entity(key=client.key(constants.boat))
-#             myRequest = {}
-#             for field in content:
-#                 new_boat.update({
-#                     field: content[field]
-#                 })
-#                 myRequest[field] = content[field]
-#             new_boat.update({
-#                 'owner': userid
-#             })
-#             myRequest['owner'] = new_boat['owner']
-#             client.put(new_boat)
-#             myRequest['id'] = str(new_boat.key.id)
-#             return(myRequest, 201)
-#         except ValueError:
-#             return("ERROR: Cannot POST /boats, check JWT", 401)
-
 
 # DELETE and PUT are added here to ensure 405 metho
 # @bp.route('', methods=['POST'])
@@ -253,21 +212,17 @@ def edit_delete_boat(boat_id):
             return (ERROR_406, 406)
 
 
-# view a boat
+# view a boat 
+# protected - only can view boat if this boat_id is in the users boats.
 @bp.route('/<boat_id>', methods=['GET'])
 def get_boat(boat_id):
-    query = client.query(kind=constants.boat)
-    results = list(query.fetch())
-    for e in results:
-        e["id"] = e.key.id
-    for e in results:
-        if str(e['id']) == str(boat_id):
-            if 'application/json' in request.accept_mimetypes:
-                return(json.dumps(e, indent=4), 200)
-            elif 'text/html' in request.accept_mimetypes:
-                res = Response(json2html.json2html.convert(json=json.dumps(e)))
-                res.headers.set('Content-Type', 'text/html')
-                return res
+    id = validate_jwt()
+    if not id:
+        return(json.dumps(ERROR_403), 403)
+    boat_key = client.key(constants.boat, int(boat_id))
+    boat = client.get(key=boat_key)
+    if boat is not None:
+        return(json.dumps(boat), 200)
     return (ERROR_404, 404)
 
     
